f91992a00db460bcd7dade4eb2f63ab607fe6b90
BohdanNosonets <nosonets07@gmail.com>
2022-08-25 12:02:33 +0300

    hw07-done

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryItemsContainer = document.querySelector('.gallery');
+const cardsMarkup = createGaleryCardsMarkup(galleryItems);
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGaleryCardsMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join('');
+}
+
+galleryItemsContainer.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  const lightboxConfig = {
+    onShow: () => {
+      document.addEventListener('keydown', onEscKeyDown);
+    },
+    onClose: () => {
+      document.removeEventListener('keydown', onEscKeyDown);
+    },
+  };
+
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const urlBigImg = evt.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <img src="${urlBigImg}" width="1200" height="600"/>
+    `,
+    lightboxConfig
+  );
+
+  instance.show();
+
+  function onEscKeyDown(evt) {
+    if (evt.code === 'Escape' && instance.visible()) {
+      instance.close();
+    }
+  }
+}
